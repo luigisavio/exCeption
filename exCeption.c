249addda88d76ee3f42e653b39acad092153b31a
luigisavio <lsavio96@gmail.com>
2025-07-12 13:24:40 +0200

    feature: add functions that throws exception. Also add functions that get (read) exception status from exception object

--- exCeption.c
+++ exCeption.c
@@ -1,40 +1,85 @@
 #include <string.h>
-// TODO check if all of these libraries are really needed
+// $TODO check if all of these libraries are really needed
 #include <stdlib.h>
 #include <stdio.h> 
 
 /*exception structure*/
 typedef struct exception {
-    exception* pParent;
-    exception* pChild;
-    int exceptionId;
+
+    int exceptionId; // numerical identifier of the exception
+
+    struct exception* pParent; // pointer to parent exception. Need to write struct since typedef has yet to happen
+    struct exception* pChild; // pointer to child exception. Need to write struct since typedef has yet to happen
+
+    char* pFunName; // pointer to first char of string that contains function name that threw this exception
+    int funNameStrLen; // length of string function name (EXCLUDING terminator char)
+
+    char* pDescription; // pointer to first char of string that contains exception description
+    int descriptionStrLen; // length of description string (EXCLUDING terminator char)
+
 } exception;
 
-/* function that creates a new exception */
-exception* throw(int exceptionId, char* exceptionName, char* exceptionDescription) {
-    // allocate memory for exception
-    void* pMemoryForException = malloc(sizeof(exception) + strlen(exceptionName) + strlen(exceptionDescription));
+/* 
+# Summary
+    Function that creates a new exception. Allocates memory, writes to it and then returns pointer.
+# Inputs
+    exceptionId
+        Numerical ID of the exception to be thrown.
+    funName
+        String containing the name of the function that is throwing the exception.
+    description
+        String containg a human understandable description of what happened.
+# Outputs
+    return
+        Pointer to new exception. 
+# Warnings    
+    Allocates memory, so the returned exception must be freed later on!
+*/
+exception* throw(int exceptionId, char* funName, char* description) {
+
+    int sizeOfException = sizeof(exception);
+    int funNameStrLen = strlen(funName);
+    int descriptionStrLen = strlen(description);
+
+    // allocate memory for main exception structure
+    exception* pException = (exception*) malloc(sizeOfException);
 
     // write to memory that was just allocated, to populate the exception
-    ((exception*)pMemoryForException)->exceptionId = exceptionId;
-    ((exception*)pMemoryForException)->pChild = NULL;
-    ((exception*)pMemoryForException)->pParent = NULL;
-    strcpy((char*)(((exception*)pMemoryForException)+1), exceptionName);
-    strcpy((char*)(((exception*)pMemoryForException)+1), exceptionDescription);
+    pException->exceptionId = exceptionId;
+    pException->pChild = NULL;
+    pException->pParent = NULL;
+    pException->funNameStrLen = strlen(funName);
+    pException->descriptionStrLen = strlen(description);
+
+    // allocate memory for function name string
+    pException->pFunName = (char*) malloc(pException->funNameStrLen+1); // +1 for terminator char, do not call another time strlen for better performance
+    // write function name to allocated memory
+    strcpy(pException->pFunName, funName); // $TODO check reutrn value
+
+    // allocate memory for exception description string
+    pException->pDescription = (char*) malloc(pException->descriptionStrLen);
+    // write to memory that was just allocated
+    strcpy(pException->pDescription, description); // $TODO check reutrn value
+
+    return pException;
 }
 
-char* exceptionToString(exception* pException) {
-    // TODO make recoursive
-    char* stringToReturn;
-    har *strcat(char *dest, const char *src);
-    return 
+int getExceptionId(exception* pException){
+    return pException->exceptionId;
 }
 
+char* getExceptionFunName(exception* pException){
+    return pException->pFunName;
+}
 
+char* getExceptionDescription(exception* pException){
+    return pException->pDescription;
+}
 
 int main(void) {
-    exception* testExcp = throw(34543, "funfun", "funfun had some problems");
-    printf(exceptionToString(testExcp));
+    exception* testExcp = throw(34543, "yunfun", "funfun had some problems");
+
+    printf("ID: %i, funName: %s, description: %s \n", getExceptionId(testExcp), getExceptionFunName(testExcp), getExceptionDescription(testExcp));
 }
 
 
