8249fe8cd38a02fa241ff6375e314771fa6e7b4d
luigisavio <lsavio96@gmail.com>
2025-08-14 08:10:28 +0200

    feat: trying better way to use exceptions

--- exCeptionTests.c
+++ exCeptionTests.c
@@ -16,7 +16,7 @@ exc_root* sumOfPositiveNumbers(int a, int b, int* c){
     }
     
 }
-// TODO try better ways to handle exceptions
+
 exc_root* averageOfNumbers(int a, int b, int* c){
 
     int result;
@@ -32,11 +32,44 @@ exc_root* averageOfNumbers(int a, int b, int* c){
     return EXC_NONE;
 }
 
+int sumOfPositiveNumbers2(int a, int b, exc_root** exc){
+
+    if (a<=0) {
+        *exc = exc_throw(1, "sumOfPositiveNumbers2", "a is negative");
+        return 0;
+    }else if (b<=0)
+    {
+        *exc = exc_throw(2, "sumOfPositiveNumbers2", "b is negative");
+        return 0;
+    } else {
+        *exc = EXC_NONE;
+        return a+b;
+    }
+    
+}
+
+int averageOfNumbers2(int a, int b, exc_root** exc){
+
+    int sum = sumOfPositiveNumbers2(a, b, exc);
+
+    if (*exc != EXC_NONE) {
+        *exc = exc_add_and_throw(*exc, 1, "averageOfNumbers2", "error in sum function");
+        return 0;
+    }else{
+        return sum/2;
+    }
+}
+
 int main(void){
     
     int c;
     exc_root* exception = averageOfNumbers(-1, 1, &c);
 
+    exc_print(exception);
+
+    exc_free(exception);
+
+    c = averageOfNumbers2(1, -1, &exception);
     exc_print(exception);
 
     exc_free(exception);
